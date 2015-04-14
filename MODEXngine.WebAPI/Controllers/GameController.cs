8d8855f76ea9b3ec37263cbaa34e50610aa6c06f
jcapellman <jcapellman@hotmail.com>
2015-04-13 21:46:48 -0400

    Added Game I/O

---
+++ MODEXngine.WebAPI/Controllers/GameController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using MODEXngine.PCL.Transports.Db;
+using MODEXngine.WebBusinessLayer.Managers;
+
+namespace MODEXngine.WebAPI.Controllers {
+    public class GameController : BaseApiController {
+        public async Task<Game> GET(Guid id) {
+            return await new GameManager().GetGame(id);
+        }
+
+        public async Task<bool> UPDATE(Game game) {
+            return await new GameManager().Update(game);
+        }
+
+        public async Task<Guid> CREATE(Game game) {
+            return await new GameManager().Create(game);
+        }
+    }
+}
---
+++ MODEXngine.WebBusinessLayer/Managers/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Data.Entity;
+using System.Data.Entity.Migrations;
+using System.Threading.Tasks;
+
+using MODEXngine.DataLayer.Contexts;
+using MODEXngine.PCL.Transports.Db;
+
+namespace MODEXngine.WebBusinessLayer.Managers {
+    public class GameManager : BaseManager {
+        public async Task<Game> GetGame(Guid id) {
+            using (var dbContext = new GameDbContext()) {
+                return await dbContext.Games.FirstOrDefaultAsync(a => a.ID == id);
+            }
+        }
+
+        public async Task<Guid> Create(Game game) {
+            using (var dbContext = new GameDbContext()) {
+                dbContext.Games.AddOrUpdate(game);
+
+                var result = await dbContext.SaveChangesAsync();
+
+                return game.ID;
+            }
+        }
+
+        public async Task<bool> Update(Game game) {
+            using (var dbContext = new GameDbContext()) {
+                dbContext.Games.AddOrUpdate(game);
+
+                return await dbContext.SaveChangesAsync() > 0;
+            }
+        }
+    }
+}
