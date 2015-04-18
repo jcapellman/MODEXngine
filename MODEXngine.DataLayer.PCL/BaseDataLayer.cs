c20af7a8c3142d277165025536d7fceebbcc97d7
jcapellman <jcapellman@hotmail.com>
2015-04-18 13:40:46 -0400

    Started on BaseDataLayer logic

---
+++ MODEXngine.DataLayer.PCL/BaseDataLayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using MODEXngine.DataLayer.PCL.Objects;
+
+namespace MODEXngine.DataLayer.PCL {
+    public abstract class BaseDataLayer {
+        public abstract T GetOne<T>(Guid id) where T : BaseObject;
+
+        public abstract Guid AddOne<T>(T obj) where T : BaseObject;
+    }
+}
