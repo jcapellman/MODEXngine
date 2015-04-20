ac3c4ab055802023ab9ad2a67dec692fee573195
jcapellman <jcapellman@hotmail.com>
2015-04-19 20:28:36 -0400

    Added SQLServerLayer project

--- MODEXngine.DataLayer.PCL/BaseDataLayer.cs
+++ MODEXngine.DataLayer.PCL/BaseDataLayer.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+
 using MODEXngine.DataLayer.PCL.Objects;
 
 namespace MODEXngine.DataLayer.PCL {
@@ -6,5 +8,9 @@ namespace MODEXngine.DataLayer.PCL {
         public abstract T GetOne<T>(Guid id) where T : BaseObject;
 
         public abstract Guid AddOne<T>(T obj) where T : BaseObject;
+
+        public abstract List<T> GetAll<T>() where T : BaseObject;
+
+        public abstract bool DeleteOne<T>(Guid id) where T : BaseObject;
     }
 }
